f832b8c6bb50c1966d0f81165672e643b2f3ef53
Paul Ledin <paul_ledin@uwalumni.com>
2024-10-02 16:54:30 -0500

    Update Affiliation_Report.py

--- Affiliation_Report.py
+++ Affiliation_Report.py
@@ -126,7 +126,9 @@ def format_currency(amount):
 ###############################################################################
 #Start building Streamlit App
 ###############################################################################
-thePassPhrase = 'PeopleNotProfit$'
+#thePassPhrase = 'PeopleNotProfit$'
+thePassPhrase = st.secrets["thePassPhrase"]
+#st.write("DB password:", st.secrets["db_password"])
 report_periods = get_report_periods_for_display()
 
 st.set_page_config(
