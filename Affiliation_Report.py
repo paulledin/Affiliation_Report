d8ebbd926181f8c76dc0e1243a32efd3de0397a8
Paul Ledin <paul_ledin@uwalumni.com>
2024-12-30 12:45:44 -0600

    Update Affiliation_Report.py

--- Affiliation_Report.py
+++ Affiliation_Report.py
@@ -73,6 +73,23 @@ def get_report_periods_from_db():
     
     return (retVal)
 
+def get_report_periods_for_display_from_db():
+    periods = pd.read_csv('https://raw.githubusercontent.com/paulledin/data/master/MonthlyReportPeriods.csv')    
+    retVal = list()
+
+    index = 0
+    for x in periods:
+        retVal.insert(index, periods[x])
+        index += 1
+        
+    df_retVal = pd.DataFrame(retVal[0])
+        
+    for i in range(len(df_retVal)):
+        period = df_retVal.loc[i, "period"]
+        df_retVal.loc[df_retVal['period'] == period, 'report_periods_formatted'] = convertDateToDisplay(str(period))
+
+    return df_retVal
+
 def get_report_periods_for_display():
     periods = pd.read_csv('https://raw.githubusercontent.com/paulledin/data/master/MonthlyReportPeriods.csv')    
     retVal = list()
