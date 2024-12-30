f8d5ee5d16579ecd2d6855c2a0c87486905f220f
Paul Ledin <paul_ledin@uwalumni.com>
2024-12-30 13:24:14 -0600

    Update Affiliation_Report.py

--- Affiliation_Report.py
+++ Affiliation_Report.py
@@ -69,7 +69,7 @@ def get_report_periods():
 @st.cache_data
 def get_report_periods_from_db():
     session = dbConn.session()   
-    return (session.sql("SELECT DISTINCT(SUBSTR(TABLE_NAME, LENGTH(TABLE_NAME)-5, length(TABLE_NAME))) FROM monthly_report.information_schema.tables WHERE table_schema!='INFORMATION_SCHEMA' ORDER BY SUBSTR(TABLE_NAME, LENGTH(TABLE_NAME)-5, LENGTH(TABLE_NAME)) DESC").to_pandas())
+    return (session.sql("SELECT DISTINCT(SUBSTR(TABLE_NAME, LENGTH(TABLE_NAME)-5, length(TABLE_NAME))) AS PERIOD FROM monthly_report.information_schema.tables WHERE table_schema!='INFORMATION_SCHEMA' ORDER BY SUBSTR(TABLE_NAME, LENGTH(TABLE_NAME)-5, LENGTH(TABLE_NAME)) DESC").to_pandas())
 
 def get_report_periods_for_display_from_db():
     session = dbConn.session()
