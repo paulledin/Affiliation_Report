fe64d0d162beead8f012cbd540aace5ac067384c
Paul Ledin <paul_ledin@uwalumni.com>
2025-05-15 15:58:12 -0500

    Update Affiliation_Report.py

--- Affiliation_Report.py
+++ Affiliation_Report.py
@@ -249,7 +249,7 @@ else:
         format="localized",),
         "Total CUs": st.column_config.NumberColumn(
         "Total # of CUs",
-        help="Number of Federally Chartered Credit Unions",
+        help="Total Number of Credit Unions",
         min_value=0,
         max_value=100000,
         step=1,
