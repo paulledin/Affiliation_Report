147d21433f40809e2e4057cea72a44444cda425d
Paul Ledin <paul_ledin@uwalumni.com>
2025-06-18 15:40:18 -0500

    Update Affiliation_Report.py

--- Affiliation_Report.py
+++ Affiliation_Report.py
@@ -182,7 +182,7 @@ def getTableAFLTable(afl_type, group_by, month, table_number):
 #Start building Streamlit App
 ###############################################################################
 report_periods = get_report_periods_for_display()
-'''
+
 with st.sidebar:
     st.markdown('![alt text](https://raw.githubusercontent.com/paulledin/data/master/ACUS.jpg)')
     passphrase = st.text_input("### Please enter the passphrase:")
@@ -338,5 +338,5 @@ else:
                          hide_index = True,
                          )   
             st.markdown('---')
-'''
+
 
